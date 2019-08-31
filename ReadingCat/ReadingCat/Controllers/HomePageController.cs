de2325a413e164812ff82b4aaad870b497c46b67
root <root@DESKTOP-BKFDVUR.localdomain>
2019-08-31 20:06:43 +0600

    UI Structures have been created

---
+++ ReadingCat/ReadingCat/Controllers/HomePageController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReadingCat.Controllers
+{
+    public class HomePageController : Controller
+    {
+        // GET: HomePage
+        public ActionResult HomePage()
+        {
+            var exemploList = new SelectList(new[] { "Exemplo1:", "Exemplo2", "Exemplo3" });
+            ViewBag.ExemploList = exemploList;
+            return View();
+        }
+
+
+    }
+}
---
+++ ReadingCat/ReadingCat/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReadingCat.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+
+
+}
