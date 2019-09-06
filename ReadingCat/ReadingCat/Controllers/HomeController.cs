4b36b84e91f0c499d2a75700686f8a162f026bda
root <root@DESKTOP-BKFDVUR.localdomain>
2019-09-06 20:31:59 +0600

    trying to create connection with database in HomeController.cs

--- ReadingCat/ReadingCat/Controllers/HomeController.cs
+++ ReadingCat/ReadingCat/Controllers/HomeController.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace ReadingCat.Controllers
 {
     public class HomeController : Controller
     {
+        string connectionString = @"Data Source =(local)\sqle2012; Initial Catalog = ReadingCat; Integrated Security = True";
+        [HttpGet]
+
         public ActionResult Index()
         {
+            DataTable dataTable = new DataTable();
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("SELECT *FROM USERS", sqlConnection);
+                sqlDataAdapter.Fill(dataTable);
+            }
             return View();
         }
 
--- ReadingCat/ReadingCat/Controllers/LoginController.cs
+++ ReadingCat/ReadingCat/Controllers/LoginController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace ReadingCat.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+        
+        [HttpGet]
         public ActionResult Login()
         {
+           
             return View();
         }
     }
