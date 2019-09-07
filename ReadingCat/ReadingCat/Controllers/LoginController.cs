8240a00c0cad5434c4bd7a1c35330b1ed2937846
root <root@DESKTOP-BKFDVUR.localdomain>
2019-09-07 12:45:01 +0600

    completed login process. After successful login, user is redirected to their profiles. If login is not successful, user stays on the same page

--- ReadingCat/ReadingCat/Controllers/LoginController.cs
+++ ReadingCat/ReadingCat/Controllers/LoginController.cs
@@ -5,18 +5,39 @@ using System.Web;
 using System.Web.Mvc;
 using System.Data;
 using System.Data.SqlClient;
+using ReadingCat.Models;
 
 namespace ReadingCat.Controllers
 {
     public class LoginController : Controller
     {
+        string connectionString = @"Data Source = DESKTOP-BKFDVUR\SQLEXPRESS; Initial Catalog = ReadingCat; Integrated Security = True";
         // GET: Login
-        
         [HttpGet]
         public ActionResult Login()
         {
-           
-            return View();
+            return View(new LoginModel());
+        }
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            DataSet dataset = new DataSet();
+            string realPassword = "";
+            string paswordFromUser = "";
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("SELECT password FROM USERS WHERE username = '"+model.username+"'", sqlConnection);
+                sqlDataAdapter.Fill(dataset);  
+                    realPassword = dataset.Tables[0].Rows[0].ItemArray[0].ToString();
+                paswordFromUser = model.password;
+             
+
+            }
+            if (realPassword == paswordFromUser)
+                return View("~/Views/Profile/Profile.cshtml");
+            else
+                return View();
         }
     }
 }
